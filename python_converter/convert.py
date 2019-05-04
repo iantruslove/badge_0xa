d88d18b7e99c2df411d2641335b8b1b17b78b987
Ian Palleiko <ian.palleiko@whiteops.com>
2019-05-04 01:34:14 -0500

    Add Arduino header

--- python_converter/convert.py
+++ python_converter/convert.py
@@ -37,36 +37,43 @@ def get_notes(mid):
     return ret
 
 with open("../src/" + files[0] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[0] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./chord1.mid")))
     f.write("}")
 
 with open("../src/" + files[1] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[1] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./chord2.mid")))
     f.write("}")
 
 with open("../src/" + files[2] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[2] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./chord3.mid")))
     f.write("}")
 
 with open("../src/" + files[3] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[3] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./bass.mid")))
     f.write("}")
 
 with open("../src/" + files[4] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[4] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./deedle1.mid")))
     f.write("}")
 
 with open("../src/" + files[5] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[5] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./deedle2.mid")))
     f.write("}")
 
 with open("../src/" + files[6] + ".c", "w") as f:
+    f.write("#include <Arduino.h>\n")
     f.write("void " + files[6] + "(uint8_t channel) {\n")
     f.write(get_notes(mido.MidiFile("./extra.mid")))
     f.write("}")
