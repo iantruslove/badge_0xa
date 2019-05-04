f5c233405d22e1a84b2962cda2110167dd7fd11a
Ian Palleiko <ian.palleiko@whiteops.com>
2019-05-04 01:25:35 -0500

    fix stuff

--- python_converter/convert.py
+++ python_converter/convert.py
@@ -8,6 +8,8 @@ T_MOD = 1277
 ON = "note_on"
 OFF = "note_off"
 
+files = ["chord1", "chord2", "chord3", "bass", "deedle1", "deedle2", "extra"]
+
 def note_to_freq(note):
     a = 440
     return (a / 32) * (2 ** ((note - 9) / 12))
@@ -17,6 +19,7 @@ def time_to_us(time):
     return us
 
 def get_notes(mid):
+    ret = ""
     for note in list(mid):
         if note.__class__ == mido.messages.messages.Message and note.type == (ON or OFF):
             freq = note_to_freq(note.note)
@@ -25,17 +28,47 @@ def get_notes(mid):
         if note.__class__ != mido.messages.messages.Message:
             continue
         elif note.type == ON:
-            print("delayMicroseconds({});".format(int(time)))
-            print("ledcWriteTone(CHANNEL, {});".format(int(freq)))
+            ret += "    delayMicroseconds({});\n".format(int(time))
+            ret += "    ledcWriteTone(channel, {});\n".format(int(freq))
         elif note.type == OFF:
-            print("delayMicroseconds({});".format(int(time)))
-            print("ledcWriteTone(CHANNEL, 0);")
+            ret += "    delayMicroseconds({});\n".format(int(time))
+            ret += "    ledcWriteTone(channel, 0);\n"
 
-if len(sys.argv) != 2:
-    print("Usage: %s <midi file>", sys.argv[0])
-    exit(2)
+    return ret
 
-FILE = sys.argv[1]
-midi_file = mido.MidiFile(FILE)
+with open("../src/" + files[0] + ".c", "w") as f:
+    f.write("void " + files[0] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./chord1.mid")))
+    f.write("}")
 
-get_notes(midi_file)
+with open("../src/" + files[1] + ".c", "w") as f:
+    f.write("void " + files[1] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./chord2.mid")))
+    f.write("}")
+
+with open("../src/" + files[2] + ".c", "w") as f:
+    f.write("void " + files[2] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./chord3.mid")))
+    f.write("}")
+
+with open("../src/" + files[3] + ".c", "w") as f:
+    f.write("void " + files[3] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./bass.mid")))
+    f.write("}")
+
+with open("../src/" + files[4] + ".c", "w") as f:
+    f.write("void " + files[4] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./deedle1.mid")))
+    f.write("}")
+
+with open("../src/" + files[5] + ".c", "w") as f:
+    f.write("void " + files[5] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./deedle2.mid")))
+    f.write("}")
+
+with open("../src/" + files[6] + ".c", "w") as f:
+    f.write("void " + files[6] + "(uint8_t channel) {\n")
+    f.write(get_notes(mido.MidiFile("./extra.mid")))
+    f.write("}")
+
+
