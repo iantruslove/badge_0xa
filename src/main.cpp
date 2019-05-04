dbf228437e4832fb08dbf2d96ee0b9ef83a226d0
Ian Truslove <ian.truslove@gmail.com>
2019-05-04 01:29:03 -0500

    Less boilerplate, more loops

--- src/main.cpp
+++ src/main.cpp
@@ -53,109 +53,42 @@ bool left_pressed, right_pressed, up_pressed, down_pressed, select_pressed;
 uint8_t left_count = 0, right_count = 0, up_count = 0, down_count = 0, select_count = 0;
 uint8_t left, right, up, down, select;
 
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
+#define SELECT 4
+#define NUM_BUTTONS 5
+
+uint8_t touchpads[] = { T3, T4, T6, T7, T2 };
+
+uint16_t touchpad_readings[] = { 0, 0, 0, 0, 0 };
+bool pressed_this_loop[] = { false, false, false, false, false };
+uint8_t debounce_count[] = { 0, 0, 0, 0, 0 };
+bool pressed[] = { false, false, false, false, false };
+
+uint8_t output_pins[] = { L_EAR, R_EAR, R_EYE, R_MUSTACHE, L_MUSTACHE };
+
 void loop()
 {
-  left = touchRead(T3);
-  right = touchRead(T4);
-  up = touchRead(T6);
-  down = touchRead(T7);
-  select = touchRead(T2);
+  for (byte n=0; n<NUM_BUTTONS; n++) {
+    touchpad_readings[n] = touchRead(touchpads[n]);
+    pressed_this_loop[n] = (touchpad_readings[n] < TOUCH_THRESHOLD);
 
-  left_pressed = (left < TOUCH_THRESHOLD);
-  right_pressed = (right < TOUCH_THRESHOLD);
-  up_pressed = (up < TOUCH_THRESHOLD);
-  down_pressed = (down < TOUCH_THRESHOLD);
-  select_pressed = (select < TOUCH_THRESHOLD);
+    if (pressed_this_loop[n]) {
+      debounce_count[n] += 1;
+    } else {
+      debounce_count[n] = 0;
+    }
 
-  if (left_pressed) {
-    left_count++;
-  } else {
-    left_count = 0;
-  }
+    pressed[n] = (debounce_count[n] > DEBOUNCE_THRESHOLD);
 
-  if (right_pressed) {
-    right_count++;
-  } else {
-    right_count = 0;
-  }
-
-  if (up_pressed) {
-    up_count++;
-  } else {
-    up_count = 0;
-  }
-
-  if (down_pressed) {
-    down_count++;
-  } else {
-    down_count = 0;
-  }
-
-  if (select_pressed) {
-    select_count++;
-  } else {
-    select_count = 0;
-  }
-
-
-
-  if (left_count > DEBOUNCE_THRESHOLD) {
-    digitalWrite(L_EAR, HIGH);
-    bass(CHANNEL);
-  } else {
-    digitalWrite(L_EAR, LOW);
-  }
-
-  if (right_count > DEBOUNCE_THRESHOLD) {
-    digitalWrite(R_EAR, HIGH);
-    chord1(CHANNEL);
-  } else {
-    digitalWrite(R_EAR, LOW);
-  }
-
-  if (up_count > DEBOUNCE_THRESHOLD) {
-    digitalWrite(R_EYE, HIGH);
-    chord2(CHANNEL);
-  } else {
-    digitalWrite(R_EYE, LOW);
-  }
-
-  if (down_count > DEBOUNCE_THRESHOLD) {
-    digitalWrite(R_MUSTACHE, HIGH);
-    chord3(CHANNEL);
-  } else {
-    digitalWrite(R_MUSTACHE, LOW);
-  }
-
-  if (select_count > DEBOUNCE_THRESHOLD) {
-    digitalWrite(L_MUSTACHE, HIGH);
-    deedle1(CHANNEL);
-  } else {
-    digitalWrite(L_MUSTACHE, LOW);
-  }
-
-  Serial.print("  left - "); Serial.print(left_pressed); Serial.print("   "); Serial.println(left);
-  Serial.print(" right - "); Serial.print(right_pressed); Serial.print("   "); Serial.println(right);
-  Serial.print("    up - "); Serial.print(up_pressed); Serial.print("   "); Serial.println(up);
-  Serial.print("  down - "); Serial.print(down_pressed); Serial.print("   "); Serial.println(down);
-  Serial.print("select - "); Serial.print(select_pressed); Serial.print("   "); Serial.println(select);
-
-  Serial.println("");
-
-  if (left_pressed || right_pressed || up_pressed || down_pressed || select_pressed) {
-    Serial.println("Something's presssed");
-  } else {
+    if (pressed[n]) {
+      digitalWrite(output_pins[n], HIGH);
+    } else {
+      digitalWrite(output_pins[n], LOW);
+    }
   }
 
   delay(30);
 }
-
-
-// Set this to be the pin that your buzzer resides in. (Note that you can only have one buzzer actively using the PWM signal at a time).
-
-void tone(int tonePin, int frequency, double msecs) {
-  ledcWriteTone(CHANNEL, frequency);
-  delayMicroseconds(int(msecs*1000));
-  ledcWriteTone(CHANNEL, 0);
-}
-
